a68dfc37a0e403802aba8b00c76a42ad9abfac46
mauriceDevsM <mauricedevs@gmail.com>
2022-03-09 20:34:00 +0300

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2022 <a href="#">National Construction Authority Staff Welfare Association Voting System</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2022 <a href="#">National Construction Authority Staff Welfare Association</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
